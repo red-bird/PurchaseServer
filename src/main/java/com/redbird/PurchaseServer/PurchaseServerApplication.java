f855ea7b4077226a4d40cb041ef1d67a85e10af7
redbird <redbird8941@gmail.com>
2021-03-03 20:49:31 +0300

    PurchaseServer initialized

---
+++ src/main/java/com/redbird/PurchaseServer/PurchaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.redbird.PurchaseServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redbird/PurchaseServer/PurchaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbird.PurchaseServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
